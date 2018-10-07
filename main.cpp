ab6fb68b336d228a1729a9b904faafb896d9ec9a
Evan Bonsignori <evanbonsignori@yahoo.com>
2018-10-06 22:42:57 -0400

    Add hardcoded test inputs to speed development. Begin decoder threads

--- main.cpp
+++ main.cpp
@@ -18,6 +18,9 @@ using namespace std;
 // Thread Methods
 void *sifter(void *);
 void *decoder(void *);
+void* fence(void*);
+void* hill(void*);
+void* valley(void*);
 // Validation Methods
 bool isValidCipher();
 // Helper/Utility Methods
@@ -28,7 +31,7 @@ ulong median(ulong a, ulong b, ulong c);
 ulong minimum(ulong a, ulong b, ulong c);
 
 // Globals
-bool is_logging = false; // True to turn on console logging for debugging purposes
+bool is_logging = true; // True to turn on console logging for debugging purposes
 string cipher_string, part_one, part_two, part_three;
 
 // ==============================
@@ -102,6 +105,8 @@ int main() {
  *   2 = user ran out of input attempts
  * */
 void* sifter(void *) {
+    uint test_index = 0;
+
     while (true) {
         cipher_string = ""; // Reset cipher_string to an empty string (for multiple runs of program)
         // ------------------------------
@@ -109,14 +114,47 @@ void* sifter(void *) {
         // ------------------------------
         bool is_valid_cipher;
         int attempts = 0;
-        int max_attempts = 3;
+        // TODO: Change back into 3 before deploying
+        int max_attempts = 13;
+
+        // TEMP
+        uint total_tests = 16;
+        string* test_inputs = new string[total_tests];
+        // Hashemi Tests
+        test_inputs[0] =  "***3 rrlmwbkaspdh 17 17 5 21 18 21 2 2 19 12 *123555eu5eotsya**3 GoodMorningJohn 3 2 1 20 15 4 10 22 3";
+        test_inputs[1] =  "**2 Global Warming 12 4 5 23 18 13 4 *** 2 RFRWYQ 5 8 17 3 4 9*31427781OAOSRSDKYPWIKSRO";
+        test_inputs[2] =  "**ACDUJF 1 4 6 12***MNKLHY 1 2 3 4 5 6 7 8 9*M FGCV";
+        test_inputs[3] =  "** KMLN 12 3 4 17 *** GHL 9 9 8 8 7 7 6 a6 5 * P TRBKHYT";
+        test_inputs[4] =  "*B PHHW PH DIVHU WKH WRJD SDUWB";
+        test_inputs[5] =  "*2 MNKK LLSGQW";
+        test_inputs[6] =  "**GG CI MOQN 9 2 1 15 ***ST TNJVQLAIVDIG 21 20 24 21 24 9 -5 21 21";
+        test_inputs[7] =  "** XNPD 5 8 17 3 ***KL LNSHDLEWMTRW 4 9 15 15 17 6 24 0 17*D GJFZNKZQDITSJ";
+        test_inputs[8] =  "**GGMLN 12 3 4 b7 *** GHL 9 9 8 8 7 7 6  16  5 * P GRBKHYT";
+        test_inputs[9] =  "** BBLN 12 3 4 17 *** GHL 9 9 8 8 7 7 6  6  5 * CRKLMNBKHYT";
+        test_inputs[10] = "** MWUULNGXAP 12 15 7 6*** GHYTRKL MNP 1 0 0 -1 2 3 12*CDM UYKOOKPIRQQN";
+        test_inputs[11] = "*zg ZHCHUUHOPXKPYAF***GHEDQHQQ 9 11 4 5 ** ABHJUTVOP 5 -2 3 4 12 32 1 0 3";
+        test_inputs[12] = "***ABC 5 6 7 8 10 1 4 8 11*K SAAPXGOW**MKPW 10 2 12 1*J RZZOWFNV";
+        // Evan Tests
+        test_inputs[13] = "**ABC 5 6 7 8 10 1 4 8 11*K SAAPXGOW**MKPW 10 2 12 1*J RZZOWFNV"; // Invalid
+        test_inputs[14] = "***ABC 5 6 7 8 10 1 4 8 11* **J RZZOWFNV"; // Invalid?
+        test_inputs[15] = "***ABC 5 6 7 8 10 1 4 8 11* t3w **"; // Invalid?
+
+
+
 
         // Prompt user to enter input cipher string until a valid string is entered or attempts run out
         while (cipher_string.empty() && attempts < max_attempts) {
             printf("Enter your cipher string of three parts, or enter 'quit' to exit program:\n");
             attempts++;
             try {
-                getline(cin, cipher_string);
+                if (test_index < total_tests) {
+                    cipher_string = test_inputs[test_index++];
+                } else {
+                    getline(cin, cipher_string);
+                }
+                if (is_logging) {
+                    printf("Entered String: \n%s\n", cipher_string.c_str());
+                }
             } catch (...) {
                 pthread_exit((void *) 1); // Exit with unsuccessful 1 code on reading input error
             }
@@ -175,7 +213,7 @@ void* sifter(void *) {
 
         if (decoder_joined == 0) {
             if (is_logging) {
-                printf("Decoder joined successfully.\n");
+                printf("Decoder thread joined successfully.\n");
                 printf("%s\n", decoder_retval); // Print return value from decoder thread
             }
         } else {
@@ -185,21 +223,47 @@ void* sifter(void *) {
     }
 }
 
-/* Splits the passed in cipher string into three parts, Part1, Part2, and Part3 that will each be passed to a separate
- * thread named fence, hill, and valley respectively. The encoding schemes that each thread follows are detailed in
- * assignment_description.pdf, a copy of which can be found in the parent directory of the repository that this
- * program belongs to */
+/* Passed Part1, Part2, and Part3 into 3 separate threads named: fence, hill, and valley respectively.
+ * The encoding schemes that each thread follows are detailed in assignment_description.pdf, a copy of
+ * which can be found in the parent directory of the repository that this program belongs to. */
 void* decoder(void*) {
-    cout << cipher_string << endl;
+    /* Note to Dr. Hashemi: It made more sense to already split the thread into three parts during validation,
+     * so rather than splitting again, this method will use the global variables containing each part to deliver them
+     * to each thread */
 
-    pthread_exit((void *)"Decoder Completed");
+
+    pthread_exit((void *)"Decoder Completed.");
+}
+
+/* Takes part_one (stored as global) of the ciphertext input and decodes it using the Fence Thread scheme defined in
+ * ./assignment_description.pdf */
+void* fence(void*) {
+
+}
+
+/* Takes part_two (stored as global) of the ciphertext input and decodes it using the Hill Thread scheme defined in
+ * ./assignment_description.pdf */
+void* hill(void*) {
+
+}
+
+/* Takes part_two (stored as global) of the ciphertext input and decodes it using the Valley Thread scheme defined in
+ * ./assignment_description.pdf */
+void* valley(void*) {
+
 }
 
 
 // ==============================
 // Validation Methods
 // ==============================
-/* Returns true if passed in cipher_string is in three parts specified by a number of asterisk symbols (6 total) */
+/* Returns true if passed in cipher_string is in three parts specified by a number of asterisk symbols (6 total)
+ *
+ * There are three phases of validation:
+ *  First Validation: There are at least one of each *, **, and *** in the cipher string
+ *  Second Validation: There are only one of each *, **, and *** in the cipher string
+ *  Third Validation: Each part is non-null
+ * */
 bool isValidCipher() {
     // Use regex to check that the cipher string contains at least 1 of each "*", "**", and "***".
     regex asterisk_regex("(?=.*?(^|[^\\*])\\*($|[^\\*]))" // Look ahead for '*' not followed/preceded by '*'
@@ -207,38 +271,53 @@ bool isValidCipher() {
                          "(?=.*?(^|[^\\*])\\*\\*\\*($|[^\\*]))"  // Look ahead for '***' not followed/preceded by '***'
                          ".*"); // All look-ahead's also account for start (^) or end ($) of string
 
-    // If string does contain one of each section
+    // First Validation: If string does contain one of each section
     if (regex_match(cipher_string, asterisk_regex)) {
         if (is_logging) {
-            printf("First validation test passed: There are at least one of each: *, **, and *** in cipher string\n");
+            printf("First validation test passed: There are at least one of each: *, **, and *** in cipher string.\n");
         }
-
-        // Verify that there is only one occurrence of each *, **, and *** (for a total of 6 occurrences of *)
-        if (std::count(cipher_string.begin(), cipher_string.end(), '*') != 6) {
-            if (is_logging) {
-                printf("Second validation Failed: There are not only one of each: *, **, and ***\n");
-            }
-            return false;
+    } else {
+        if (is_logging) {
+            printf("First validation test failed: There are not at least one of each: *, **, and *** in cipher string.\n");
         }
+        return false;
+    }
 
-        // Verify that splitting the string into three parts gives 3 non-null parts
-        splitIntoParts();
+    // Second Validation: Verify that there is only one occurrence of each *, **, and *** (for a total of 6)
+    if (std::count(cipher_string.begin(), cipher_string.end(), '*') != 6) {
+        if (is_logging) {
+            printf("Second validation Failed: There are not only one of each: *, **, and ***.\n");
+        }
+        return false;
+    } else {
+        if (is_logging) {
+            printf("Second validation test passed: There are only one of each: *, **, and *** in cipher string.\n");
+        }
+    }
 
+    // Verify that splitting the string into three parts gives 3 non-null parts
+    splitIntoParts();
+
+    if (is_logging) {
         printf("Part1: %s\n", part_one.c_str());
         printf("Part2: %s\n", part_two.c_str());
         printf("Part3: %s\n", part_three.c_str());
+    }
 
+    // Third Validation: Verify that each part is non-null
+    if (part_one.length() <= 0 || part_two.length() <= 0 || part_three.length() <= 0) {
         if (is_logging) {
-            printf("Second validation test passed: There are only one of each: *, **, and *** in cipher string\n");
+            printf("Third validation test failed: There are null values for at least one part.\n");
+            return false;
         }
-        return true;
+    } else {
+        if (is_logging) {
+            printf("Third validation test passed: Each part is non-null.\n");
+        }
     }
 
-
-    if (is_logging) {
-        printf("First validation test failed: There are not at least one of each: *, **, and *** in cipher string\n");
-    }
-    return false;
+    // If all validation tests pass, return true
+    return true;
 }
 
 
